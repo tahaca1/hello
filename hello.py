c3d5f5183d63bf4d27cf164aef03c8a55ca94694
taha <tahabenrayanaca@gmail.com>
2024-11-19 22:55:38 +0100

    first python code

---
+++ hello.py
@@ -0,0 +1,9 @@
+# Demander le nom de l'utilisateur
+nom = input("Quel est ton nom ? ")
+
+# Demander l'âge de l'utilisateur
+age = int(input("Quel est ton âge ? "))
+
+# Afficher un message personnalisé
+print(f"Bonjour {nom}, tu as {age} ans !")
+
